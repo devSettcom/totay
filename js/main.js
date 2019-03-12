8b710a2ad15b0b32340381fffd7e94833eaee494
devSettcom <toteesett@gmail.com>
2019-03-12 15:02:05 -0700

    Totay2.0
    Re-did code and added nav bar and more pages

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ $('#view-work').on('click', function(){
         scrollTop: me
     }, 900);
 });
+
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+}
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+}
+function changeColor(){
+    document.getElementById('one').style.stroke = '#5B5F63';
+    document.getElementById('two').style.stroke = '#5B5F63';
+    document.getElementById('three').style.stroke = '#5B5F63';
+}
+function revertColor(){
+    document.getElementById('one').style.stroke = '#312F2F';
+    document.getElementById('two').style.stroke = '#312F2F';
+    document.getElementById('three').style.stroke = '#312F2F';
+}
